cb489b8da269693c51ce433af57d7930a242bb7b
Abhishek-CGI <125307055+Abhishek-CGI@users.noreply.github.com>
2023-02-14 02:08:29 +0530

    Update streamlit_app.py
    Added text and emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
